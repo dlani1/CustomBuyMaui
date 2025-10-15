b5566be1ff2c9197e309c574cabcb6dbd03c52ba
dani1uwu <131733168+dani1uwu@users.noreply.github.com>
2025-10-14 22:14:57 -0600

    Primer commit de proyecto .NET MAUI

---
+++ CustomBuyMaui/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CustomBuyMaui;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ CustomBuyMaui/InicioPage.xaml.cs
@@ -0,0 +1,27 @@
+namespace CustomBuyMaui;
+
+public partial class InicioPage : ContentPage
+{
+    // ... constructor, etc.
+
+    // **Asegúrate de que estos 4 métodos existan:**
+    private void OnCatalogoClicked(object sender, EventArgs e)
+    {
+        // Lógica para el botón Catálogo
+    }
+
+    private void OnPersonalizarClicked(object sender, EventArgs e)
+    {
+        // Lógica para el botón Personalizar
+    }
+
+    private void OnPedidosClicked(object sender, EventArgs e)
+    {
+        // Lógica para el botón Pedidos
+    }
+
+    private void OnAjustesClicked(object sender, EventArgs e)
+    {
+        // Lógica para el botón Ajustes
+    }
+}
